ee680d1c3b55fb787bc8cf79fa39d795976c03fe
exvxe <bareiden@gmail.com>
2019-02-12 20:01:38 +0100

    skills added

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -3,17 +3,3 @@ let resetScroll = () => {
 }
 
 window.setTimeout(resetScroll, 1000);
-
-let gridItems = document.getElementsByClassName("grid-item");
-
-
-let gridClickHandler = (index) => {
-    gridItems[index].classList.toggle("toggled");
-}
-
-for (let index = 0; index < gridItems.length; index++) {
-    gridItems[index].addEventListener("click", () => {
-        gridClickHandler(index)
-    }, false)
-    
-}
