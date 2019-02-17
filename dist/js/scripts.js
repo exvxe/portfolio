34983af538a37803a61316da566dfe0ecb6aa9e9
exvxe <bareiden@gmail.com>
2019-02-17 18:21:13 +0100

    circle chart without labels

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -4,35 +4,54 @@ let resetScroll = () => {
 
 window.setTimeout(resetScroll, 1000);
 
-    
-var json = [
-    { "name": "Cezary", "age": 25 },
-    { "name": "Franek", "age": 11 },
-    { "name": "Bartek", "age": 29 },
-];
 
-function createAList(data) {
-    d3.select(".techs")
-    .append("ul")
-    .selectAll("li")
-    .data(data)
-    .enter()
-    .append("li")
-    .text(function (d) {
-        return d.name + ": " + d.age;
-    })
-}
+let arcData = [
+                {label: 'HTML5', value: 7},
+                {label: 'CSS3', value: 9},
+                {label: 'Stylus', value: 8},
+                {label: 'D3.js', value: 4},
+                {label: 'Photoshop', value: 10}
+            ],
+    color =             d3.scaleLinear()
+                        .domain([1,100])
+                        .interpolate(d3.interpolateHcl)
+                        .range([d3.rgb("#393E41"), d3.rgb('#FF0707')]),
+    arcGenerator =      d3.arc()
+                        .innerRadius(50)
+                        .outerRadius(100);
 
-function changeList() {
-    d3.selectAll("li")
-        .style("font-weight", function (d) {
+arcData = d3.pie()
+            .value(function(d) { return d.value; })
+            (arcData);
 
-            if (d.age < 21) {
-                return "normal";
-            } else {
-                return "bold";
-            }
-    })
-}
+d3.select('g')
+.selectAll('path')
+.data(arcData)
+.enter()
+.append('path')
+.attr("fill", function(d) { return color(d.value*14) })
+.attr('d', arcGenerator)
+.attr("id", function(d,i) { return "skillsArcSlice_"+i; })
+.each(function(d,i) {
+    var firstArcSection = /(^.+?)L/;
 
-createAList(json);
+    var newArc = firstArcSection.exec( d3.select(this).attr("d") )[1];
+    newArc = newArc.replace(/,/g , " ");
+
+    d3.select('g').append("path")
+        .attr("class", "hiddenSkillsArcs")
+        .attr("id", "skillsArc"+i)
+        .attr("d", newArc)
+        .style("fill", "none");
+});
+
+
+d3.select('g')
+.data(arcData)
+.enter().append("text")
+.attr("class", "skillsText")
+.append("textPath")
+.attr("startOffset","50%")
+.style("text-anchor","middle")
+.attr("xlink:href",function(d,i){return "#skillsArc"+i;})
+.text(function(d){return d.data.label;});
