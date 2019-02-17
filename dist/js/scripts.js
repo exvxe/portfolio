efabd15c8279e8decb6d06c3d9613914242affee
exvxe <bareiden@gmail.com>
2019-02-17 09:52:52 +0100

    d3

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -4,32 +4,35 @@ let resetScroll = () => {
 
 window.setTimeout(resetScroll, 1000);
 
+    
 var json = [
     { "name": "Cezary", "age": 25 },
     { "name": "Franek", "age": 11 },
     { "name": "Bartek", "age": 29 },
 ];
 
-var pi = Math.PI;
-    
-let arc = (d) => {
-    d3.arc()
-    .innerRadius(50)
-    .outerRadius(70)
-    .startAngle(0 * (pi/180)) //converting from degs to radians
-    .endAngle(d.age * (pi/180)) //just radians
+function createAList(data) {
+    d3.select(".techs")
+    .append("ul")
+    .selectAll("li")
+    .data(data)
+    .enter()
+    .append("li")
+    .text(function (d) {
+        return d.name + ": " + d.age;
+    })
 }
-    
-    
-    
 
-d3.select(".techs")
-.attr("width", "400")
-.attr("height", "400")
-.append("svg")
-.data(json)
-.enter()
-.append("path")
-.arc()
-.attr("fill", "red")
-.attr("transform", "translate(200,200)");
+function changeList() {
+    d3.selectAll("li")
+        .style("font-weight", function (d) {
+
+            if (d.age < 21) {
+                return "normal";
+            } else {
+                return "bold";
+            }
+    })
+}
+
+createAList(json);
