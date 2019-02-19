9f7c3e1423ee169cd40bf248905d44d94895c1d3
exvxe <bareiden@gmail.com>
2019-02-19 20:09:37 +0100

    testing new scheme

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,19 +6,24 @@ window.setTimeout(resetScroll, 1000);
 
 
 let arcData = [
+                {label: 'Research', value: 10},
                 {label: 'HTML5', value: 7},
+                {label: 'Javascript', value: 6},
+                {label: 'jQuery', value: 4},
+                {label: 'D3.js', value: 3},
                 {label: 'CSS3', value: 9},
                 {label: 'Stylus', value: 8},
-                {label: 'D3.js', value: 4},
+                {label: 'Sass', value: 7},
+                {label: 'Bootstrap', value: 6},
                 {label: 'Photoshop', value: 10}
             ],
-    color = [ 'F2806A', '#F17259', 'F06449', 'DB5B43', '#C5D4AF', '#BECFA5', '#B7CA9B', '#B0C592']
+    color = [ '#D5A021', '#2F333C', '#181C26', '#010611', '#010610', '#C5D4AF', '#BECFA5', '#B7CA9B', '#B0C592']
     arcGenerator =      d3.arc()
                         .innerRadius(120)
-                        .outerRadius(160);
+                        .outerRadius(200);
 
 arcData = d3.pie()
-            .value(function(d) { return d.value; })
+            .value(function(d) { return d.value; }).sort(null)
             (arcData);
 
 d3.select('g')
@@ -26,7 +31,7 @@ d3.select('g')
 .data(arcData)
 .enter()
 .append('path')
-.attr("fill", function(d) { return color[d.value] })
+.attr("fill", function(d) { return color[d.index] })
 .attr('d', arcGenerator)
 .attr("id", function(d,i) { return "skillsArcSlice_"+i; })
 .each(function(d,i) {
@@ -34,7 +39,6 @@ d3.select('g')
 
     var newArc = firstArcSection.exec( d3.select(this).attr("d") )[1];
     newArc = newArc.replace(/,/g , " ");
-
     d3.select('g').append("path")
         .attr("class", "hiddenSkillsArcs")
         .attr("id", "skillsArc"+i)
