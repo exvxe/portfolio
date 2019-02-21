9f585f98cfc56c611e41406b18990e76dc2585e8
exvxe <bareiden@gmail.com>
2019-02-21 20:24:05 +0100

    finally defined direction of development

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -4,20 +4,15 @@ let resetScroll = () => {
 
 window.setTimeout(resetScroll, 1000);
 
+let headerText = document.querySelector('.header_wrapper > h1')
 
-HTML5
-CSS
-JavaScript
-TypeScript
-JSON
-XML
-Wordpress
-Git
-_lodash
-Bootstrap
-SASS
-jQuery
-Gulp
-npm
-Webpack
-BEM
+let headerTextSplited = headerText.innerText.split(' ');
+headerText.innerHTML = headerTextSplited.map(function(x) {
+    return "<span class='word'>" + x + "</span> ";
+}).join("");
+
+document.querySelector('.word:nth-of-type(1)').outerHTML += "<br>"
+
+let headerLastWord = document.querySelector('.word:nth-last-of-type(1)')
+
+headerLastWord.innerHTML = "<a href='#techs'>" + headerLastWord.innerText + "</a>"
