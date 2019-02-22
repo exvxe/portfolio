3f593dde00d54772e9214e3df167fe59a72efe71
exvxe <bareiden@gmail.com>
2019-02-22 18:39:49 +0100

    techs

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -4,15 +4,19 @@ let resetScroll = () => {
 
 window.setTimeout(resetScroll, 1000);
 
-let headerText = document.querySelector('.header_wrapper > h1')
+let splitter = () => {
+    let headerText = document.querySelector('.header_wrapper > h1')
 
-let headerTextSplited = headerText.innerText.split(' ');
-headerText.innerHTML = headerTextSplited.map(function(x) {
-    return "<span class='word'>" + x + "</span> ";
-}).join("");
+    let headerTextSplited = headerText.innerText.split(' ');
+    headerText.innerHTML = headerTextSplited.map(function(x) {
+        return "<span class='word'>" + x + "</span> ";
+    }).join("");
+    
+    document.querySelector('.word:nth-of-type(1)').outerHTML += "<br>"
+    
+    let headerLastWord = document.querySelector('.word:nth-last-of-type(1)')
+    
+    headerLastWord.innerHTML = "<a href='#techs'>" + headerLastWord.innerText + "</a>"
+}
 
-document.querySelector('.word:nth-of-type(1)').outerHTML += "<br>"
-
-let headerLastWord = document.querySelector('.word:nth-last-of-type(1)')
-
-headerLastWord.innerHTML = "<a href='#techs'>" + headerLastWord.innerText + "</a>"
+splitter()
