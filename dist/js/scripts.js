efdd73b505c885821de4a237045196320f87a7f2
exvxe <bareiden@gmail.com>
2019-02-25 19:56:26 +0100

    little refactoring

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -2,11 +2,9 @@ let resetScroll = () => {
     window.scrollTo(0, 0);
 }
 
-window.setTimeout(resetScroll, 1000);
-
 let splitter = () => {
     let headerText = document.querySelector('.header_wrapper > h1')
-
+    
     let headerTextSplited = headerText.innerText.split(' ');
     headerText.innerHTML = headerTextSplited.map(function(x) {
         return "<span class='word'>" + x + "</span> ";
@@ -16,12 +14,18 @@ let splitter = () => {
     
 }
 
-splitter()
-
-let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-window.addEventListener('resize', () => {
+let mobileHeight = () => {
     let vh = window.innerHeight * 0.01;
+    
     document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+    window.addEventListener('resize', () => {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });    
+}
+
+let startFunction = () => {
+    window.setTimeout(resetScroll, 1000);
+    splitter();
+    mobileHeight();
+}
